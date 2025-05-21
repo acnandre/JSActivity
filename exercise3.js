f53ea16c02435f3bed77fa93de4f2ff0428a20d4
Andre Matthew Dumandan <andre.m.dumandan@accenture.com>
2025-05-21 06:38:08 +0000

    change: exercise 3

--- exercise3.js
+++ exercise3.js
@@ -1,1 +1,13 @@
-git
+function xSign(height){
+    for (let i = 0; i < height; i++) {
+        let row = '';
+        for (let j = 0; j < height; j++) {
+            if (j === i || j === height - 1 - i) {
+                row += '*';
+            } else {
+                row += ' ';
+            }
+        }
+        console.log(row);
+    }
+};
